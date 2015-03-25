fad727ee0277de141f56f08fdd876f4b82f61536
smartkiwi <vladimir.vladimirov@magnetic.com>
2015-03-25 14:28:31 -0400

    Update cert_tests.c
    removing this test

--- tests/cert_tests.c
+++ tests/cert_tests.c
@@ -16,31 +16,6 @@ char *test_SSL_verify_cert()
 
     int ret = 0;
 
-    ret =x509_crt_parse_file( &crt, "tests/ca/certs/m2-cert.pem" );
-
-    mu_assert(ret == 0, "failed to parse cert m2-cert.pem");
-
-    ret =x509_crt_parse_file( &ca_crt, "tests/ca/none.pem" );
-
-    mu_assert(ret != 0, "failed to fail on non-existent pem none.pem");
-
-    ret =x509_crt_parse_file( &ca_crt, "tests/ca/cacert.pem" );
-
-    mu_assert(ret == 0, "failed to parse cert cacert.pem");
-
-    ret =x509_crl_parse_file( &crl, "tests/ca/crl.pem" );
-
-    mu_assert(ret == 0, "failed to parse cert crl.pem");
-
-    int flags = 0;
-    ret =x509_crt_verify( &crt, &ca_crt, NULL, NULL, &flags, NULL, NULL);
-
-    mu_assert(ret == 0, "failed to verify cert m2-cert.pem");
-
-    x509_crt_free( &crt );
-    x509_crt_free( &ca_crt );
-    x509_crl_free( &crl );
-
     return NULL;
 }
 
